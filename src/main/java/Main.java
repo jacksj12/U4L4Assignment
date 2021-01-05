3944ca696c29ddedfe2cc6d63b098ab8b5579c76
jacksj12 <30806811+jacksj12@users.noreply.github.com>
2021-01-05 07:59:39 -0800

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // you can print static methods by using Class.method() format
+        System.out.println("Hello World!");
+    }
+}
