4d356cad5be53188bfa81e2eb694eb0c570beea1
jacksj12 <30806811+jacksj12@users.noreply.github.com>
2021-01-05 07:55:34 -0800

    Update Primes.java

--- src/main/java/Primes.java
+++ src/main/java/Primes.java
@@ -1,24 +1,19 @@
 public class Primes {
     public static int countFactors(int n){
         int factors = 0;
-        for(int i = 1; i <= n; i++){
-            if (n % i == 0)
-                factors++;
-        }
+        // replace with your code
         return factors;
     }
 
     public static boolean isPrime(int n){
         boolean prime = true;
-        prime = countFactors(n) == 2;
+        // replace with your code
         return prime;
     }
 
     public static int countPrimes(int n){
         int numPrimes = 0;
-        for(int i = 2; i < n; i++)
-            if (isPrime(i))
-                numPrimes++;
+        // replace with your code
         return numPrimes;
     }
 }
